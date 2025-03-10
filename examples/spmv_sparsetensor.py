92470e93d4f1c87cee3902899b5a57fcd7c119ab
Brian Kelley <bmkelle@sandia.gov>
2025-03-10 16:25:30 -0600

    Clean up spmv_sparsetensor, copy NewSparseTensorFactory
    Signed-off-by: Brian Kelley <bmkelle@sandia.gov>

--- examples/spmv_sparsetensor.py
+++ examples/spmv_sparsetensor.py
@@ -17,33 +17,25 @@ class SpMV(torch.nn.Module):
         return torch.mv(A, x)
 
 def main():
-    #Asp = mmread('bodyy5.mtx').tocsr()
-    #m = Asp.shape[0]
-    #n = Asp.shape[1]
-    #A = torch.sparse_csr_tensor( \
-    #        torch.tensor(Asp.indptr, dtype=torch.int32), \
-    #        torch.tensor(Asp.indices, dtype=torch.int32), \
-    #        torch.tensor(Asp.data, dtype=torch.float))
-
     rowptrs = np.array([0, 1, 5, 6, 8, 8], dtype=np.int32)
     colinds = np.array([1, 0, 2, 3, 4, 2, 0, 1], dtype=np.int32)
-    values = np.array([1.1, 0.3, 2.2, 3.7, -4, -19, -2, 1], dtype=np.float32)
+    values = np.array([1.1, 0.3, 2.2, 3.7, -4, -19, -2, 1], dtype=np.double)
     A = torch.sparse_csr_tensor( \
             torch.tensor(rowptrs, dtype=torch.int32), \
             torch.tensor(colinds, dtype=torch.int32), \
-            torch.tensor(values, dtype=torch.float))
+            torch.tensor(values, dtype=torch.double))
     m = 5
     n = 5
 
-    x = torch.ones((n), dtype = torch.float)
-    y = torch.ones((m), dtype = torch.float)
+    x = torch.ones((n), dtype = torch.double)
+    y = torch.ones((m), dtype = torch.double)
 
     module_torch = SpMV()
     module_torch.train(False)
 
     # Construct the SparseTensorStorage for A
-    #Asp = ctypes.pointer(newSparseTensorFactory()((m,n), np.float32, postype=np.int32, crdtype=np.int32, buffers=[rowptrs, colinds, values], levelFormats=[LevelFormat.Dense, LevelFormat.Compressed]))
-    Asp = newSparseTensorFactory()((m,n), np.float32, postype=np.int32, crdtype=np.int32, buffers=[rowptrs, colinds, values], levelFormats=[LevelFormat.Dense, LevelFormat.Compressed])
+    #Asp = ctypes.pointer(newSparseTensorFactory()((m,n), np.double, postype=np.int32, crdtype=np.int32, buffers=[rowptrs, colinds, values], levelFormats=[LevelFormat.Dense, LevelFormat.Compressed]))
+    Asp = newSparseTensorFactory()((m,n), np.double, postype=np.int32, crdtype=np.int32, buffers=[rowptrs, colinds, values], levelFormats=[LevelFormat.Dense, LevelFormat.Compressed])
 
     # Use MPACT/TorchFX to export the torch module while maintaining sparsity
     # (torchscript, which we use for dense examples can't do this)
@@ -52,7 +44,7 @@ def main():
     module_kokkos = backend.compile(module_linalg)
 
     print("y = Ax from torch:")
-    print(module_torch.forward(A, x))
+    print(module_torch.forward(A, x).numpy())
     print("y = Ax from kokkos:")
     print(module_kokkos.lapis_main(Asp, x.numpy()))
 
